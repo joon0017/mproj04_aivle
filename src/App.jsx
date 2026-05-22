02b31849d3cd69d6d22eb4a6e1250d847e2a0753
Kimdohoon <163837627+rlaehgns123@users.noreply.github.com>
2026-05-22 15:46:38 +0900

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+    <div className="page">
+      <header className="header">
+        <h1>대충 프로젝트 별명</h1>
+
+        <nav className="nav">
+          <span>크트츠</span>
+          <span>크트츠</span>
+          <span>내 정보</span>
+          <div className="profile">🙄</div>
+          <button>로그아웃</button>
+        </nav>
+      </header>
+
+      <section className="search-area">
+        <select>
+          <option>전체</option>
+        </select>
+
+        <button className="category-btn">카테고리</button>
+
+        <input className="search-input" />
+
+        <button className="search-btn">검색</button>
       </section>
 
-      <div className="ticks"></div>
+      <main className="content">
+        <section className="book-list">
+          <BookItem />
+          <BookItem />
+        </section>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        <aside className="side-box">
+          <div className="side-title">최근 검색</div>
+          <div className="side-empty"></div>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+          <div className="side-title">sort</div>
+          <ul>
+            <li>서명순</li>
+            <li>저자순</li>
+            <li>발행년도(1 - 9)</li>
+            <li>발행년도(9 - 1)</li>
+          </ul>
+
+          <div className="side-title">구분</div>
+
+          <div className="filter-title">자료유형</div>
+          <ul>
+            <li>단행본</li>
+          </ul>
+
+          <div className="filter-title">저자</div>
+          <ul>
+            <li>...</li>
+          </ul>
+
+          <div className="filter-title">출판사</div>
+          <ul>
+            <li>...</li>
+          </ul>
+
+          <div className="filter-title">출판년도</div>
+          <ul>
+            <li>2020</li>
+          </ul>
+        </aside>
+      </main>
+    </div>
+  );
 }
 
-export default App
+function BookItem() {
+  return (
+    <div className="book-item">
+      <div className="cover">
+        표지
+        <br />
+        img
+      </div>
+
+      <div className="book-info">
+        <h2>제목</h2>
+        <div className="line"></div>
+
+        <div className="desc">
+          txt
+          <button className="detail-btn">소장정보</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
